2670cac205045a2a26cb67ff40d7b002483ff5ba
Seu Nome <seu@email.com>
2026-04-18 17:09:43 +0200

    Fix: menu mobile fecha automaticamente ao clicar nos links

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-
 const menu = document.querySelector(".menu-toggle");
 const nav = document.querySelector(".nav-links");
 
-menu.addEventListener("click", ()=>{nav.classList.toggle("active");
-                                    menu.classList.toggle("active")});
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("active");
+});
 
 
-const links = document.querySelectAll(".nav-links a");
+const links = document.querySelectorAll(".nav-links a");
+
+
 links.forEach(link => {
-
-    link.addEventListener("click", ()=>{
-
-        nav.classList.remove("active");
-    });
-    
+  link.addEventListener("click", () => {
+    nav.classList.remove("active");
+  });
 });
